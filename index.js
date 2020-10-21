0c37ef2eafa4d98af03ba1f3551b973e8774b29b
wzdmriv <wzdmriv@gmail.com>
2020-10-21 11:38:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function errorCallback(err) {
 
 $(function(){
     $(window).load(function(){
-        $('#li').each(function(i){
+        $('.contents li').each(function(i){
             $(this).delay(i * 100).css({visibility:'visible',opacity:'0'}).animate({opacity:'1'},1000);
         });
     });
