8f6eb5b77e2ef26070c8166476456d5c4201b560
wzdmriv <wzdmriv@gmail.com>
2020-10-21 12:45:14 +0900

    1

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function(){
     $(window).load(function(){
-        $('.fadein li').each(function(i){
+        $('ul.fadein li').each(function(i){
             $(this).delay(i * 100).css({visibility:'visible',opacity:'0'}).animate({opacity:'1'},1000);
         });
     });
