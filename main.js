de2780b5acda6a06216a5b1cb82849f8f6e3adf0
wzdmriv <wzdmriv@gmail.com>
2020-10-21 12:32:40 +0900

    1

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function(){
     $(window).load(function(){
-        $('.contents li').each(function(i){
+        $('.fadein li').each(function(i){
             $(this).delay(i * 100).css({visibility:'visible',opacity:'0'}).animate({opacity:'1'},1000);
         });
     });
