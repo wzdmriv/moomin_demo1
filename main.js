7b4b9b9f1858c5047e7e5c7d003071cee7df27ca
wzdmriv <wzdmriv@gmail.com>
2020-10-21 13:34:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
     // 設定
     
     var interval =3000; // 切り替わりの間隔（ミリ秒）
-    var fade_speed = 1000;// フェード処理の早さ（ミリ秒）
+    var fade_speed = 500;// フェード処理の早さ（ミリ秒）
     $(".fade-img-box img").hide();
     $(".fade-img-box img:first").addClass("active").show();
     var changeImage = function(){
