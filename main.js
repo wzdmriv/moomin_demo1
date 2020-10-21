9ff96efa68931db1605ec735eefed8d4151c2cea
wzdmriv <wzdmriv@gmail.com>
2020-10-21 13:11:12 +0900

    1

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-$(function(){
-    $(window).load(function(){
-        $('#ul.fadein li').each(function(i){
-            $(this).delay(i * 100).css({visibility:'visible',opacity:'0'}).animate({opacity:'1'},1000);
-        });
-    });
-});
+(function(){
+    // 設定
+    var interval =3000; // 切り替わりの間隔（ミリ秒）
+    var fade_speed = 1000;// フェード処理の早さ（ミリ秒）
+    $(".fade-img-box img").hide();
+    $(".fade-img-box img:first").addClass("active").show();
+    
+    var changeImage = function(){
+            var $active = $(".fade-img-box img.active");
+            var $next = $active.next("img").length?$active.next("img"):$(".fade-img-box img:first");
+       
+            $active.fadeOut(fade_speed).removeClass("active");
+            $next.fadeIn(fade_speed).addClass("active");
+    }
+    
+    
+    setInterval(changeImage,interval);
+    }());
