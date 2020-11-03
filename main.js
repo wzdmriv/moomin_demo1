b210d8837e638281d62d24b1d259ef69ff200c1b
wzdmriv <wzdmriv@gmail.com>
2020-11-03 15:10:20 +0900

    Update main.js

--- main.js
+++ main.js
@@ -22,15 +22,5 @@ window.onload = function(){
     var interval =3000; // 切り替わりの間隔（ミリ秒）
     var fade_speed = 500;// フェード処理の早さ（ミリ秒）
     $(".fade-img-box img").hide();
-    $(".fade-img-box img:first").addClass("active").show();
-    var changeImage = function(){
-            var $active = $(".fade-img-box img.active");
-            var $next = $active.next("img").length?$active.next("img"):$(".fade-img-box img:first");
-       
-            $active.fadeOut(fade_speed).removeClass("active");
-            $next.fadeIn(fade_speed).addClass("active");
-            console.log("hello")
-    }
     
-    $(document).ready(setInterval(changeImage,interval));
 }());
