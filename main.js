9e256837c2ebfd80b469131ee82564df21883049
wzdmriv <wzdmriv@gmail.com>
2020-11-08 00:59:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
-// 画像用配列
-var images = [
-    'prt1.png',
-    'prt2.png',
-    'prt3.png',
-    'prt4.png',
-    'prt5.png',
-    'prt6.png'
-];
-
-
-
 (function(){
     // 設定
     
-    var interval =3000; // 切り替わりの間隔（ミリ秒）
+    var interval =10000; // 切り替わりの間隔（ミリ秒）
     var fade_speed = 500;// フェード処理の早さ（ミリ秒）
     $("#img").hide();
     $(".fade-img-box img:first").addClass("active").show();
     var changeImage = function(){
             var $active = $(".fade-img-box img.active");
-            var $next = $active.next("img").length?$active.next("img"):$(".fade-img-box img:first");
+            var $next = $active.next("img").length?$active.next("img");
        
             $active.fadeOut(fade_speed).removeClass("active");
             $next.fadeIn(fade_speed).addClass("active");
