c83b3c2ee1b9cd757bc23a920f878d836f6db77e
wzdmriv <wzdmriv@gmail.com>
2020-11-08 01:34:01 +0900

    1

--- main.js
+++ main.js
@@ -7,11 +7,12 @@
     $(".fade-img-box img:first").addClass("active").show();
     var changeImage = function(){
             var $active = $(".fade-img-box img.active");
-            var $next = $active.next("img").length?$active.next("img");
-       
-            $active.fadeOut(fade_speed).removeClass("active");
-            $next.fadeIn(fade_speed).addClass("active");
-            console.log("hello")
+            if($active.next("img").length){
+                var $next = $active.next("img").length?$active.next("img"):$(".fade-img-box img:first");
+                $active.fadeOut(fade_speed).removeClass("active");
+                $next.fadeIn(fade_speed).addClass("active");
+                console.log("hello")
+            } 
     }
     
     $(document).ready(setInterval(changeImage,interval));
